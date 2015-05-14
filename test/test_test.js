aa6480c6b1b837f96f82bc2a1a6a073311659dc5
Tri Nguyen <mail@trimn.net>
2015-05-14 22:27:10 +0200

    Update tests

--- test/test_test.js
+++ test/test_test.js
@@ -1,8 +1,13 @@
 var assert = require('assert')
 
-describe('Command', function () {
-  it ('should throw error when no command is provided', function () {
-    var value = 1
-    assert.equal(value, 1)
-  })
+describe('Test', function () {
+  it ('Should pass', function () {
+    var value = 1;
+    assert.equal(value, 1);
+  });
+
+  it ('Should fail', function () {
+    var value = 2;
+    assert.equal(value, 1);
+  });
 })
