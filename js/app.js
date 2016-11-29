8c4cc58e528e22110aa0789b86894202a058f915
Tri Nguyen <mail@trimn.net>
2016-11-29 21:27:09 +0100

    Refactor app.js for newer react/omniscient++

--- js/app.js
+++ js/app.js
@@ -1,74 +1,55 @@
-import React from 'react';
-import omniscient from 'omniscient';
-import immstruct from 'immstruct';
-import Router from 'react-router';
+import D from 'react-dom';
+import component from 'omniscient';
+import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
 
-import RouterHelper from './router-helper';
-import data from './SomeData';
-
-const component = omniscient.withDefaults({
-  jsx: true
-});
-
-const { Route, RouteHandler, DefaultRoute, Link } = Router;
+import observer from 'omnipotent/decorator/observer';
+import data from './data';
 
 
 /* Components
 ------------------------------------------------------------------------------*/
-const Menu = component('Menu', _ => {
-  return (
-    <ul>
-      <li><Link to="test">Test</Link></li>
-      <li><Link to="test2">Test2</Link></li>
-    </ul>
-  );
-});
+const TestComponent1 = component('TestComponent1', ({ test }) => (
+  <div>
+    <p>test: { test.deref() }</p>
+  </div>
+));
 
-const TestComponent1 = component('TestComponent1', () => {
-  return (<div><p>test</p></div>);
-});
-
-const TestComponent2 = component('TestComponent2', () => {
-  return (<div><p>test2</p></div>);
-});
+const TestComponent2 = component('TestComponent2', ({ test2 }) => (
+  <div>
+    <p>test2: { test2.deref() }</p>
+  </div>
+));
 
 
 /* Application
 ------------------------------------------------------------------------------*/
-const App = component('App',
-  {
-    shouldComponentUpdate: () => {
-      return true;
-    }
-  },
-  function () {
-    return (
-      <div>
-        <h2>omniscient-router-test</h2>
-        <Menu />
-        <RouteHandler {...this.props} />
-      </div>
-    );
-  }
+const App = ({ children }) => (
+  <div>
+    <h2>omniscient-router-test</h2>
+
+    <ul>
+      <li><Link to="test">Test</Link></li>
+      <li><Link to="test2">Test2</Link></li>
+    </ul>
+
+    { children }
+  </div>
 );
 
 
 /* Routers
 ------------------------------------------------------------------------------*/
-const RouterTest1 = RouterHelper(TestComponent1, data, 'test');
-const RouterTest2 = RouterHelper(TestComponent2, data, 'test2');
+const RouterTest1 = observer(data, { test: ['test'] }, TestComponent1);
+const RouterTest2 = observer(data, { test2: ['test2'] }, TestComponent2);
 
-const routes = (
-  <Route handler={App}>
-    <DefaultRoute handler={RouterTest1} />
-    <Route name="test" handler={RouterTest1} />
-    <Route name="test2" handler={RouterTest2} />
-  </Route>
+D.render(
+  <Router history={ hashHistory }>
+    <Route path="/" component={ App }>
+      <IndexRoute component={ RouterTest1 } />
+
+      <Route path="test" component={ RouterTest1 } />
+      <Route path="test2" component={ RouterTest2 } />
+    </Route>
+  </Router>,
+  document.getElementById('mainContent')
 );
-
-Router.run(routes, (Handler, state) => {
-  React.render(
-    <Handler cursor={data.cursor()} statics={state} />,
-    document.getElementById('mainContent')
-  );
-});
